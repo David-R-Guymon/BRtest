327dc67a0f4f6f636952e67efd56650e24604b31
David-R-Guymon <David.Guymon@basicresearch.org>
2015-10-29 10:32:58 -0600

    added image
    added a new image and center div tag.

--- index.php
+++ index.php
@@ -6,10 +6,12 @@
 <title>Test Title</title>
 </head>
 <body>
-
+<div align="center">
 <h1>This is the Title</h1>
     
     <p>This is only a test.</p>
-
+    
+    <img src="myproducts3b.png" />
+</div>
 </body>
 </html>
