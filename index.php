862a0431a1e901f2aa5106e16547a30be2c75f0f
David-R-Guymon <David.Guymon@basicresearch.org>
2015-10-29 10:29:18 -0600

    Main page
    added main page.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Zantrex3 - SkinnyStix</title>
+</head>
+<body>
+
+<h1>This is the Title</h1>
+    
+    <p>This is only a test.</p>
+
+</body>
+</html>
